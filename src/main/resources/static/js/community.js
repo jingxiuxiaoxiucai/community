f6615786c4dd398b79e545268f03c57eb65cd1d4
jingixuxiaoxiucai <631348014@qq.com>
2020-04-07 20:20:55 +0800

    pom文件的profiles

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -28,7 +28,7 @@ function comment2target(targetId, type, content) {
                 if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
                     if (isAccepted) {
-                        window.open("https://github.com/login/oauth/authorize?client_id=2859958f9f059979ed3a&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
+                        window.open("https://github.com/login/oauth/authorize?client_id=649b3b1f0069b57c6a75&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
                         window.localStorage.setItem("closable", true);
                     }
                 } else {
